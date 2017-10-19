5a48133f69e30a8cb5fde437768d4b13d358cf4a
Samantha Pendleton <sap21@aber.ac.uk>
2017-10-19 14:59:27 +0100

    Updated prac 3

--- csm0120/03/readwords.py
+++ csm0120/03/readwords.py
@@ -5,12 +5,13 @@ Created on Thu Oct 19 10:11:58 2017
 
 @author: sap21
 """
-
+'''
 myfile = open("words.txt")
 #print(myfile) #prints info
 for line in myfile:
     print(line.strip())
 myfile.close()
+'''
 '''
 myfile = open("words.txt")
 for line in myfile.readlines(): #readlines returns items as a list
@@ -27,7 +28,7 @@ while line:
 myfile.close()
 '''
 print()
-
+'''
 myfile = open("words.txt")
 for line in myfile:
     if line.startswith('a'):
@@ -44,7 +45,7 @@ for line in uni_file:
     if len(module) != 4:
         print(module)
 uni_file.close()
-
+'''
 print()
 print() # CSV: lines separated by commas
 print()
@@ -56,9 +57,10 @@ for line in csvreader:
     print("entry: ", line) # each line is a list 
     print("mod_code: ", line[0]) # 0 element
     print("title: ", line[1])
-    print("length: ", len(line))
+    print("length of columns: ", len(line))
+    #if '2' not in line[3]:
+    #    print(line.count('1'))
+    if "'Mr Neil Taylor'" in line[2]:
+        print("neil taylors modules: ", line[0])
     print()
 my_csv.close()
-
-# counts how many modules taught in semester 1
-# only prints out module codes that Neil Taylor runs
