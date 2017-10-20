27152261d847bd720b69f1167c41c0095c4bca97
Samantha Pendleton <sap21@aber.ac.uk>
2017-10-20 14:08:38 +0100

    added prac 03

---
+++ csm0120/03/prac_03.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 13:04:44 2017
+
+@author: sap21
+"""
+
+# exercise 1
+
+grades = {}
+total_students = input("total entries...")
+
+for g in range(0,(int(total_students))):
+    name = input("enter student name...")
+    percent = input("enter student grade...")
+    while int(percent) < 0:
+        percent = input("percentage incorrect, please enter a new one...")
+    while int(percent) > 100:
+        percent = input("percentage incorrect, please enter a new one...")
+    grades[name] = int(percent)
+print()
+print(grades)
+print()
+print("sum of all grades: ", sum(grades.values()))
+mean_grades = (sum(grades.values()))/(int(total_students))
+print("mean of grades: ", mean_grades)
+print()
+stu_keys = list(grades.keys())
+stu_val = list(grades.values())
+print(stu_keys, stu_val)
+
+s = 0
+for s in grades:
+    print(grades.key(s))
+    s = int(s) + 1
+    
