30291ed47a25ddd5d42475458bee1ec92870446e
Samantha Pendleton <samanfapendle@outlook.com>
2017-10-26 21:52:15 +0100

    updated prac 03

--- csm0120/prac_03.py
+++ csm0120/prac_03.py
@@ -9,7 +9,7 @@ Created on Sat Oct 21 13:14:21 2017
 #redoing practical 3
 
 #exercise 1
-
+'''
 grades = {}
 entries = input("entries...")
 for g in range(int(entries)): #range(0,5)
@@ -38,7 +38,8 @@ while ins != "stop":
         print("not in system")
     else:
         print(grades[ins])
-
+'''
+print("######################################################################")
 
 #exercise 2
 
@@ -49,3 +50,38 @@ def happy_birthday(age):
         print("another year older")
     elif age <= 20:
         print("congrats you are", age, "years old")
+        
+print("######################################################################")        
+        
+#exercise 3
+
+import random
+def generate():
+    dna = []
+    for c in range(0,101):
+        c = random.choice(['A','C','G','T'])
+        dna.append(c)
+    return "".join(dna)
+
+dna = generate()
+print(dna)
+
+def compliment(dna):
+    dna_base = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+    compliment = []
+    for base in dna:
+        base = dna_base[base]
+        compliment.append(base)
+    return "".join(compliment)
+
+print("|" * len(dna)) #exercise.4
+print(compliment(dna))
+    
+print("######################################################################")
+    
+#exercise 4
+
+print("######################################################################")
+    
+#exercise 5
+
