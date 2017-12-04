dc5142eff146433efbed667e22f8d2e7d35e2a1f
Samantha Pendleton <samanfapendle@outlook.com>
2017-12-04 19:20:17 +0000

    add kmer counts and fasta files

--- 002/kmer-counts.py
+++ 002/kmer-counts.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 alphabet = "ACGNT"
 
@@ -18,13 +18,10 @@ def perms(chars, k):
                 result.append( c+p )
         return result
 
-
 def initialise_kmer_dict(chars, k):
     '''Initialise dictionary with zeros for each possible k-mer'''
     ps = perms(chars, k)
     return dict.fromkeys(ps, 0)
-
-
 
 def distance(list1, list2):
     '''Find the distance between two lists of numbers'''
@@ -33,16 +30,11 @@ def distance(list1, list2):
         total += abs(n1-n2)
     return total
 
-
-
 def normalise(kmers):
     '''Normalise each value by dividing it by the total.'''
     total = sum (kmers.values())
     for k in kmers:
         kmers[k] = kmers[k] / total
-
-
-
 
 def addcounts(filename, k, species_kmers):
     '''Add the k-mer counts for a k-mer size of k, from given file'''
@@ -61,8 +53,6 @@ def addcounts(filename, k, species_kmers):
     species = filename.split('.')[0] # first part of filename
     species_kmers[species] = d
 
-
-   
 def inorder_vals(d):
     '''Return the values in an order sorted by the keys'''
     return [d[k] for k in sorted(d.keys())] 
@@ -76,7 +66,7 @@ if __name__ == "__main__":
             print("Reading " + filename)
             addcounts(filename, k, species_kmers)
     print(k)
-    for sp1 in sorted(species_kmers):
+    '''for sp1 in sorted(species_kmers):
         vals1 = inorder_vals(species_kmers[sp1])
         print(vals1)
         plt.bar(range(len(vals1)), vals1)
@@ -85,3 +75,4 @@ if __name__ == "__main__":
         for sp2 in sorted(species_kmers):
             vals2 = inorder_vals(species_kmers[sp2])
             print("%.8f %s %s" % (distance(vals1,vals2),sp1,sp2))
+            '''
