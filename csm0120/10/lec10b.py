f19648d7b9efeaf2a8a5a105d8bc99e8c5c7056a
Samantha Pendleton <samanfapendle@outlook.com>
2017-12-07 16:59:09 +0000

    Lec 10b

---
+++ csm0120/10/lec10b.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 10:02:15 2017
+
+@author: sap21
+"""
+
+# terminal
+# mkdir < make dire
+# git clone https://........
+
+# less helloworld.py
+# /main < to search in less
+# q < quit
+
+'''
+gedit README.md
+git status -s 
+git add README.md
+git status -s
+git commit -m “Added my first text to the README.md file”
+git status -s
+git push
+git log
+git checkout e3b4a710c656e41619eb6b5ac039b1ad740d89ff README.md
+git pull
+
+'''
