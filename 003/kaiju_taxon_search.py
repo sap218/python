f9270323fca5152348106a13bbf75e3185a98986
Samantha Pendleton <samanfapendle@outlook.com>
2018-07-22 13:37:18 +0100

    updated to print out info - trying to get insert csv to work

--- 003/kaiju_taxon_search.py
+++ 003/kaiju_taxon_search.py
@@ -3,16 +3,22 @@
 Created on Sat Jul 21 14:19:49 2018
 @author: samantha
 """
-
 import requests
 import bs4
 from bs4 import BeautifulSoup
+import csv
+
+def insert_csv(file):
+    with open(file, newline='') as csvfile:
+        #reader = csv.reader(csvfile, delimiter=' ')
+        reader = csv.DictReader(file)
+...     for row in reader:
+...         print(row['seqid'], row['taxon'])
 
 def search(taxon):
     url = "https://www.ncbi.nlm.nih.gov/taxonomy/?"
     payload = {"term":taxon}
     response = requests.get(url, params=payload)
-    
     if response.status_code == 200:
         return response.text
     else:
@@ -24,24 +30,22 @@ def print_html(html_doc):
     
 def soup_tag(soup):
     results = []    
-    
-    tag = soup.find("p", {"class": "title"})
     try:
-        for t in tag:
-                results.append(tag)
-    except (NameError, TypeError):
+        tag = soup.find("p", {"class": "title"}).contents[0].contents[0]
+        results.append(tag)
+    except (NameError, TypeError, IndexError):
         print("bad response from NCBI")
-    tag = soup.find("div", {"class": "rprt", "class": "supp"})
     try:
-        for t in tag:
-                results.append(tag)
-    except (NameError, TypeError):
+        tag = soup.find("div", {"class": "rprt", "class": "supp"}).contents[0].contents[0]
+        results.append(tag)
+    except (NameError, TypeError, IndexError):
         print("bad response from NCBI")
-
     return results
 
 
 if __name__ == "__main__":
+    insert_csv("/home/samantha/Dissertation/kaiju_allC_seqid_taxon.csv")
+    
     response = search("1797182")
     if response is None:
         print("bad response from NCBI")
@@ -49,8 +53,6 @@ if __name__ == "__main__":
         soup = print_html(response)
     #html_content = soup.prettify()  
     #print(html_content)
-     
     tag = soup_tag(soup)
     for t in tag:
-        tag.append(str(t))
-        #print(str(t))
+        print(t)
